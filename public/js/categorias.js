5487da0f9b3f54f0796fdc1768c3eee90f08316f
andressa-mb <andressamesquita26@gmail.com>
2025-02-06 21:19:41 -0300

    Criação das Categorias e Livros e criação das tabelas de favoritos e comentários

--- public/js/categorias.js
+++ public/js/categorias.js
@@ -1,31 +1,3 @@
-/* document.addEventListener("DOMContentLoaded", function () {
-    const form = document.getElementById("formCadastroCategorias");
-    if (form) {
-        form.addEventListener("submit", function (e) {
-            let nome = document.getElementById("inputNomeCat").value.trim();
-            let descricao = document
-                .getElementById("inputDescricao")
-                .value.trim();
-            let errors = [];
-            if (nome.length < 3) {
-                errors.push(
-                    "O nome da categoria deve ter pelo menos 3 caracteres."
-                );
-            }
-            if (descricao.length > 100) {
-                errors.push(
-                    "O nome da descrição deve ter no máximo 100 caracteres."
-                );
-            }
-            if (errors.length > 0) {
-                e.preventDefault();
-                alert(errors.join("\n"));
-            }
-        });
-    }
-});
- */
-
 document.addEventListener("DOMContentLoaded", function () {
     console.log("carregou a pagina delete");
     const btnsExcluir = document.querySelectorAll("a[data-toggle='modal']");
